9da108b9aba7b509794ecd522a27c9ab38199bd8
ZZY <jxygzzy@163.com>
2020-07-27 20:33:56 +0800

    QQbot

--- Bot/plugins/news/data_source.py
+++ Bot/plugins/news/data_source.py
@@ -7,7 +7,7 @@
 import requests
 
 
-url='http://c.m.163.com/nc/article/headline/T1348649580692/0-40.html'
+url='https://api.apiopen.top/getWangYiNews'
 headers={
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
 }
@@ -17,9 +17,9 @@ async def get_news():
     text=res.json()
     count=0;
     result=''
-    for i in text['T1348649580692']:
+    for i in text['result']:
         result=result+i['title']+'\n'
-        result=result+i['url']+'\n'
+        result=result+i['path']+'\n'
         count=count+1
         if count==10:
             break
